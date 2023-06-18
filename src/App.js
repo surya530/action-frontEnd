87460ae8edff0dc5d89a297f02b3fdd3a2985805
suryahk <tejaksurya@gmail.com>
2023-06-18 21:17:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
+import { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+let [data , setData] = useState([]);
+
+
+  useEffect(()=>{
+    axios.get('http://localhost:3002/data').then((res)=>{
+      let {data} = res;
+      setData(data.data);
+    }).catch((err)=>{
+
+    })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {
+          data.map((e,index)=>{
+              return(
+                <p key={index}>name : {e.name} , age : {e.age}</p>
+              )
+          })
+        }
     </div>
   );
 }
